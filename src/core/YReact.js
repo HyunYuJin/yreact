fb71de0c22563fbbd3445ba10b2ccf22f334c334
HyeonYuJin <gusdbwls9772@gmail.com>
2022-08-16 00:30:01 +0900

    feat: useState currentStateKey에 대한 고민 console

--- src/core/YReact.js
+++ src/core/YReact.js
@@ -9,11 +9,15 @@ function YReact () {
 
   function useState (initState) {
     const { currentStateKey: key, states } = options
+    
+    console.log(states.length, key, states)
+    // 0부터 접근할 수 있게 하는 이유는 initState를 넣냐 마냐를 위한 것이고,
+    // key를 찾아갈 수 있는건 클로저의 개념을 대입해봤을 때 해당 컴포넌트에 key 값이 죽지 않고 살아있기 때문이다...?
+
     if (states.length === key) {
       states.push(initState)
     }
 
-    console.log(states.length, key, states)
     
     const setState = (newState) => {
       if (states[key] === newState) {
